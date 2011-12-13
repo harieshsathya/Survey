7a6e5d455b9e0a38fd63a5cf59db7eb398f0c6bd
Sathya <harieshsathya@gmail.com>
2011-12-12 23:41:03 -0500

    Modeled working survey elephant

--- main.py
+++ main.py
@@ -10,27 +10,32 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
 
+class UserInfo(db.Model):
+  """Models the users info with id, name and email"""
+  user = db.UserProperty(required=True,auto_current_user_add=True)
 
-class Greeting(db.Model):
-  """Models an individual Guestbook entry with an author, content, and date."""
-  author = db.UserProperty()
-  content = db.StringProperty(multiline=True)
+class Survey(db.Model):
+  """Models a relationship between survey_id and author"""
+  survey_id = db.IntegerProperty(required=True)
+  survey_name = db.StringProperty(required=True)
+  user = db.UserProperty(required=True,auto_current_user_add=True)
   date = db.DateTimeProperty(auto_now_add=True)
 
+class SurveyDesc(db.Model):
+  """Models an individual Guestbook entry with an author, content, and date."""
+  survey_id = db.IntegerProperty(required=True)
+  question = db.StringProperty(multiline=True)
+  answers = db.ListProperty(str)
 
-def guestbook_key(guestbook_name=None):
+def Survey_Desc_Key(user_name=None):
   """Constructs a datastore key for a Guestbook entity with guestbook_name."""
-  return db.Key.from_path('Guestbook', guestbook_name or 'default_guestbook')
+  return db.Key.from_path('Survey', user_name or 'Default_Survey')
 
 
 class MainPage(webapp.RequestHandler):
   def get(self):
-        guestbook_name=self.request.get('guestbook_name')
-        greetings_query = Greeting.all().ancestor(
-            guestbook_key(guestbook_name)).order('-date')
-        greetings = greetings_query.fetch(10)
-
-        if users.get_current_user():
+        User = users.get_current_user()
+        if User:
             url = users.create_logout_url(self.request.uri)
             url_linktext = 'Logout'
         else:
@@ -38,7 +43,6 @@ class MainPage(webapp.RequestHandler):
             url_linktext = 'Login'
 
         template_values = {
-            'greetings': greetings,
             'url': url,
             'url_linktext': url_linktext,
         }
@@ -47,26 +51,27 @@ class MainPage(webapp.RequestHandler):
         self.response.out.write(template.render(path, template_values))
 
 
-class Guestbook(webapp.RequestHandler):
+class Survey(webapp.RequestHandler):
   def post(self):
     # We set the same parent key on the 'Greeting' to ensure each greeting is in
     # the same entity group. Queries across the single entity group will be
     # consistent. However, the write rate to a single entity group should
     # be limited to ~1/second.
-    guestbook_name = self.request.get('guestbook_name')
-    greeting = Greeting(parent=guestbook_key(guestbook_name))
+    pass
+#    guestbook_name = self.request.get('guestbook_name')
+#    greeting = Greeting(parent=guestbook_key(guestbook_name))
 
-    if users.get_current_user():
-      greeting.author = users.get_current_user()
+#    if users.get_current_user():
+#      greeting.author = users.get_current_user()
 
-    greeting.content = self.request.get('content')
-    greeting.put()
-    self.redirect('/?' + urllib.urlencode({'guestbook_name': guestbook_name}))
+#    greeting.content = self.request.get('content')
+#    greeting.put()
+#    self.redirect('/?' + urllib.urlencode({'guestbook_name': guestbook_name}))
 
 
 application = webapp.WSGIApplication([
   ('/', MainPage),
-  ('/sign', Guestbook)
+  ('/my_survey', Survey)
 ], debug=True)
 
 
