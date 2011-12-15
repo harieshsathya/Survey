d11781f2493b97e4c10738f6a8e006ee33528389
Sathya <harieshsathya@gmail.com>
2011-12-15 01:37:32 -0500

    Adding questions

--- main.py
+++ main.py
@@ -27,31 +27,34 @@ class SurveyDesc(db.Model):
   question = db.StringProperty(multiline=True)
   answers = db.ListProperty(str)
 
-def Survey_Desc_Key(user_name=None):
+def Survey_Key(user_email=None):
   """Constructs a datastore key for a Guestbook entity with guestbook_name."""
-  return db.Key.from_path('Survey', user_name or 'Default_Survey')
+  return db.Key.from_path('SurveyKey', user_email or 'default')
 
 
 class MainPage(webapp.RequestHandler):
   def get(self):
-        User = users.get_current_user()
-        if User:
-            url = users.create_logout_url(self.request.uri)
-            url_linktext = 'Logout'
-        else:
-            url = users.create_login_url(self.request.uri)
-            url_linktext = 'Login'
+    current_user = users.get_current_user()
+    if current_user:
+      current_user_info = UserInfo()
+      current_user_info.user = current_user;
+      current_user_info.put()
+      url = users.create_logout_url(self.request.uri)
+      url_linktext = 'Logout'
+    else:
+      url = users.create_login_url(self.request.uri)
+      url_linktext = 'Login'
 
-        template_values = {
-            'url': url,
-            'url_linktext': url_linktext,
-        }
+    template_values = {
+      'url': url,
+      'url_linktext': url_linktext,
+    }
 
-        path = os.path.join(os.path.dirname(__file__), 'index.html')
-        self.response.out.write(template.render(path, template_values))
+    path = os.path.join(os.path.dirname(__file__), 'index.html')
+    self.response.out.write(template.render(path, template_values))
 
 
-class Survey(webapp.RequestHandler):
+class MySurvey(webapp.RequestHandler):
   def post(self):
     # We set the same parent key on the 'Greeting' to ensure each greeting is in
     # the same entity group. Queries across the single entity group will be
@@ -69,9 +72,69 @@ class Survey(webapp.RequestHandler):
 #    self.redirect('/?' + urllib.urlencode({'guestbook_name': guestbook_name}))
 
 
+class CreateSurvey(webapp.RequestHandler):
+  def get(self):
+    current_user = users.get_current_user()
+    if current_user:
+      url = users.create_logout_url(self.request.uri)
+      url_linktext = 'Logout'
+    else:
+      url = users.create_login_url(self.request.uri)
+      url_linktext = 'Login'
+
+    template_values = {
+      'url': url,
+      'url_linktext': url_linktext,
+    }
+
+    path = os.path.join(os.path.dirname(__file__), 'create_survey.html')
+    self.response.out.write(template.render(path, template_values))
+
+class AllSurvey(webapp.RequestHandler):
+  def post(self):
+    pass
+
+class Results(webapp.RequestHandler):
+  def post(self):
+    pass
+
+class AddSurvey(webapp.RequestHandler):
+  def get(self):
+    current_user = users.get_current_user()
+    next_survey_id = 1
+    if current_user:
+      survey_query = Survey.all().ancestor(Survey_Key(current_user.email())).order('-date')
+      last_survey = survey_query.fetch(1)
+      for survey in last_survey:
+        next_survey_id = survey.survey_id + 1
+      new_survey = Survey(
+          parent=Survey_Key(current_user.email()),
+          survey_id=next_survey_id,
+          survey_name=self.request.get('survey_name')
+          )
+      new_survey.put()
+      url = users.create_logout_url(self.request.uri)
+      url_linktext = 'Logout'
+    else:
+      url = users.create_login_url(self.request.uri)
+      url_linktext = 'Login'
+
+    template_values = {
+      'url': url,
+      'url_linktext': url_linktext,
+      'survey_id': next_survey_id,
+    }
+
+    path = os.path.join(os.path.dirname(__file__), 'add_question.html')
+    self.response.out.write(template.render(path, template_values))
+
 application = webapp.WSGIApplication([
   ('/', MainPage),
-  ('/my_survey', Survey)
+  ('/my_survey', MySurvey),
+  ('/all_survey', AllSurvey),
+  ('/create', CreateSurvey),
+  ('/add_survey', AddSurvey),
+  ('/view_completed', Results)
 ], debug=True)
 
 
