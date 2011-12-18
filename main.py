da0a3d336b2ccabad2a6d76f074dee180d2eac4c
Sathya <harieshsathya@gmail.com>
2011-12-17 20:33:45 -0500

    Voting and Viewing results compleed

--- main.py
+++ main.py
@@ -24,12 +24,28 @@ class Survey(db.Model):
 class SurveyDesc(db.Model):
   """Models an individual Guestbook entry with an author, content, and date."""
   survey_id = db.StringProperty(required=True)
+  qno = db.IntegerProperty()
   question = db.StringProperty(multiline=True)
   answers = db.ListProperty(str)
+
+class Votes(db.Model):
+  survey_id = db.StringProperty()
+  qno = db.IntegerProperty()
+  option1 = db.IntegerProperty(default=0)
+  option2 = db.IntegerProperty(default=0)
+  option3 = db.IntegerProperty(default=0)
+  option4 = db.IntegerProperty(default=0)
+
+class UserVoteRecord(db.Model):
+  survey_id = db.StringProperty()
+  users = db.ListProperty(users.User)
 
 def Survey_Key(user_email=None):
   """Constructs a datastore key for a Guestbook entity with guestbook_name."""
   return db.Key.from_path('SurveyKey', user_email or 'default')
+
+def question_key(survey_id=None):
+  return db.Key.from_path('QuestionKey', survey_id or 'default')
 
 
 class MainPage(webapp.RequestHandler):
@@ -86,7 +102,7 @@ class ViewMySurvey(webapp.RequestHandler):
   def get(self):
     current_user = users.get_current_user()
     if current_user:
-      survey_desc_query = SurveyDesc.all().ancestor(Survey_Key(current_user.email()))
+      survey_desc_query = SurveyDesc.all().ancestor(question_key(self.request.get('group1')))
       all_survey_desc = survey_desc_query.fetch(200)
       current_survey = []
       for survey in all_survey_desc:
@@ -116,6 +132,135 @@ class ViewMySurvey(webapp.RequestHandler):
     path = os.path.join(os.path.dirname(__file__), 'display_current_survey.html')
     self.response.out.write(template.render(path, template_values))
 
+class VoteSurvey(webapp.RequestHandler):
+  def get(self):
+    current_user = users.get_current_user()
+    if current_user:
+      survey_query = Survey.all().order('-date')
+      all_survey = survey_query.fetch(200)
+      url = users.create_logout_url(self.request.uri)
+      url_linktext = 'Logout'
+    else:
+      url = users.create_login_url(self.request.uri)
+      url_linktext = 'Login'
+
+    template_values = {
+      'url': url,
+      'url_linktext': url_linktext,
+      'surveys': all_survey,
+    }
+
+    path = os.path.join(os.path.dirname(__file__), 'vote_survey.html')
+    self.response.out.write(template.render(path, template_values))
+
+
+class VoteMySurvey(webapp.RequestHandler):
+  def get(self):
+    current_user = users.get_current_user()
+    if current_user:
+      survey_desc_query = SurveyDesc.all().ancestor(question_key(self.request.get('group1')))
+      all_survey_desc = survey_desc_query.fetch(2000)
+      current_survey = []
+      for survey in all_survey_desc:
+        if survey.survey_id == str(self.request.get('group1')):
+          current_survey.append(survey)
+
+      survey_query = Survey.all()
+      all_survey = survey_query.fetch(2000)
+      for survey in all_survey:
+        date = str(survey.date)
+        if(date == str(self.request.get('group1'))):
+          current_survey_name = survey.survey_name
+
+      url = users.create_logout_url(self.request.uri)
+      url_linktext = 'Logout'
+    else:
+      url = users.create_login_url(self.request.uri)
+      url_linktext = 'Login'
+
+    template_values = {
+      'url': url,
+      'url_linktext': url_linktext,
+      'surveys': current_survey,
+      'survey_name': current_survey_name,
+      'survey_id': self.request.get('group1'),
+    }
+
+    path = os.path.join(os.path.dirname(__file__), 'vote_current_survey.html')
+    self.response.out.write(template.render(path, template_values))
+
+class RegisterVote(webapp.RequestHandler):
+  def get(self):
+    current_user = users.get_current_user()
+    current_survey_id = self.request.get('survey_id')
+    if current_user:
+      survey_desc_query = SurveyDesc.all().ancestor(question_key(current_survey_id))
+      all_survey_desc = survey_desc_query.fetch(2000)
+      current_survey = []
+      for survey in all_survey_desc:
+        if survey.survey_id == current_survey_id:
+          current_survey.append(survey)
+
+      for survey in current_survey:
+        number=str(survey.qno)
+        answer=self.request.get(number)
+        i=1
+        
+        for ans in survey.answers:
+          if ans == answer:
+            break
+          else:
+            i = i + 1
+        
+        existing_vote_query = Votes.all().ancestor(question_key(current_survey_id))
+        existing_vote = existing_vote_query.fetch(2000)
+
+        flag=True
+        for vote in existing_vote:
+          if vote.qno == survey.qno:
+            flag = False
+            if i==1:
+              vote.option1 = vote.option1 + 1
+            if i==2:
+              vote.option2 = vote.option2 + 1
+            if i==3:
+              vote.option3 = vote.option3 + 1
+            if i==4:
+              vote.option4 = vote.option4 + 1
+            vote.put()
+
+        if flag:
+          new_vote = Votes(parent=question_key(current_survey_id))
+          new_vote.qno=survey.qno
+          new_vote.survey_id=current_survey_id
+          if i==1:
+            cv = new_vote.option1
+            new_vote.option1 = cv + 1
+          if i==2:
+            cv = new_vote.option2
+            new_vote.option2 = cv + 1
+          if i==3:
+            cv = new_vote.option3
+            new_vote.option3 = cv + 1
+          if i==4:
+            cv = new_vote.option4
+            new_vote.option4 = cv + 1
+          new_vote.put()
+        
+      url = users.create_logout_url(self.request.uri)
+      url_linktext = 'Logout'
+    else:
+      url = users.create_login_url(self.request.uri)
+      url_linktext = 'Login'
+
+    template_values = {
+      'url': url,
+      'url_linktext': url_linktext,
+      'survey_name': "name",
+    }
+
+    path = os.path.join(os.path.dirname(__file__), 'index.html')
+    self.response.out.write(template.render(path, template_values))
 
 class CreateSurvey(webapp.RequestHandler):
   def get(self):
@@ -153,14 +298,14 @@ class AllSurvey(webapp.RequestHandler):
       'surveys': all_survey,
     }
 
-    path = os.path.join(os.path.dirname(__file__), 'display_survey.html')
+    path = os.path.join(os.path.dirname(__file__), 'display_all_survey.html')
     self.response.out.write(template.render(path, template_values))
 
 class ViewAllSurvey(webapp.RequestHandler):
   def get(self):
     current_user = users.get_current_user()
     if current_user:
-      survey_desc_query = SurveyDesc.all()
+      survey_desc_query = SurveyDesc.all().ancestor(question_key(self.request.get('group1')))
       all_survey_desc = survey_desc_query.fetch(2000)
       current_survey = []
       for survey in all_survey_desc:
@@ -192,8 +337,60 @@ class ViewAllSurvey(webapp.RequestHandler):
 
 
 class Results(webapp.RequestHandler):
-  def post(self):
-    pass
+  def get(self):
+    current_user = users.get_current_user()
+    if current_user:
+      survey_query = Survey.all().order('-date')
+      all_survey = survey_query.fetch(200)
+      url = users.create_logout_url(self.request.uri)
+      url_linktext = 'Logout'
+    else:
+      url = users.create_login_url(self.request.uri)
+      url_linktext = 'Login'
+
+    template_values = {
+      'url': url,
+      'url_linktext': url_linktext,
+      'surveys': all_survey,
+    }
+
+    path = os.path.join(os.path.dirname(__file__), 'view_results_survey.html')
+    self.response.out.write(template.render(path, template_values))
+
+class ShowResults(webapp.RequestHandler):
+  def get(self):
+    current_user = users.get_current_user()
+    current_survey_id = self.request.get('group1')
+    if current_user:
+      survey_desc_query = SurveyDesc.all().ancestor(question_key(current_survey_id))
+      all_survey_desc = survey_desc_query.fetch(2000)
+      current_survey = []
+      for survey in all_survey_desc:
+        if survey.survey_id == current_survey_id:
+          current_survey.append(survey)
+
+      result_query = Votes.all().ancestor(question_key(current_survey_id))
+      results = result_query.fetch(2000)
+      current_results = []
+      for result in results:
+        if result.survey_id == current_survey_id:
+          current_results.append(result)
+
+      url = users.create_logout_url(self.request.uri)
+      url_linktext = 'Logout'
+    else:
+      url = users.create_login_url(self.request.uri)
+      url_linktext = 'Login'
+
+    template_values = {
+      'url': url,
+      'url_linktext': url_linktext,
+      'surveys': current_survey,
+      'results': current_results,
+    }
+
+    path = os.path.join(os.path.dirname(__file__), 'show_results.html')
+    self.response.out.write(template.render(path, template_values))
 
 class AddQuestion(webapp.RequestHandler):
   def get(self):
@@ -201,10 +398,17 @@ class AddQuestion(webapp.RequestHandler):
     
     if current_user:
       new_survey_desc = SurveyDesc(
-          parent=Survey_Key(current_user.email()),
+          parent=question_key(self.request.get('survey_id')),
           survey_id=self.request.get('survey_id'),
           question=self.request.get('question')
           )
+
+      current_qno=1
+      get_survey_desc = SurveyDesc.all().ancestor(question_key(self.request.get('survey_id'))).order('-survey_id').order('-qno')
+      get_survey = get_survey_desc.fetch(1)
+      for survey in get_survey:
+        if survey.survey_id == self.request.get('survey_id'):
+          current_qno=survey.qno+1
     
       option_list = []
       option1 = self.request.get('option1')
@@ -222,6 +426,7 @@ class AddQuestion(webapp.RequestHandler):
       
 
       new_survey_desc.answers = option_list
+      new_survey_desc.qno = current_qno
       new_survey_desc.put()
     
       url = users.create_login_url(self.request.uri)
@@ -296,7 +501,11 @@ application = webapp.WSGIApplication([
   ('/create', CreateSurvey),
   ('/add_survey', AddSurvey),
   ('/view_completed', Results),
-  ('/add_question', AddQuestion)
+  ('/show_result', ShowResults),
+  ('/add_question', AddQuestion),
+  ('/vote', VoteSurvey),
+  ('/vote_my_survey', VoteMySurvey),
+  ('/vote_current', RegisterVote)
 ], debug=True)
 
 
